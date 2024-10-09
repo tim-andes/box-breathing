ac5726fde860adaa7a6f4c915c2c326bf0e36c0b
TIMOTHY ANDES <tim.andes+git@gmail.com>
2024-10-08 22:40:04 -0600

    initial setup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="parent-container">
+      {/* Header */}
+      <div className="header-container">
+        <header className="app-header">
+          <h1>Box Breathing</h1>
+          <h2>Just breathe...</h2>
+        </header>
+      </div>
+      {/* Body */}
+      <div className="body-container"></div>
+      <div className="grid-container">
+          <div className="cell cell1"></div>
+          <div className="cell cell2"></div>
+          <div className="cell cell3"></div>
+          <div className="cell cell4"></div>
+          <div className="cell cell5"></div>
+          <div className="cell cell6"></div>
+          <div className="cell cell7"></div>
+          <div className="cell cell8"></div>
+          <div className="cell cell9"></div>
+          <div className="cell cell10"></div>
+          <div className="cell cell11"></div>
+          <div className="cell cell12"></div>
+          <div className="cell cell3"></div>
+          <div className="cell cell14"></div>
+          <div className="cell cell15"></div>
+          <div className="cell cell16"></div>
+        </div>
+      </div>
   );
 }
 
